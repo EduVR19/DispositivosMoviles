5b5cb3301d792d47775c811691abafec0ab42f2a
BurlierBarley <86444168+BurlierBarley@users.noreply.github.com>
2021-07-03 10:14:10 -0500

    Ahora el usuario no puede hacer una reservación, si ya tiene una

--- app/src/main/java/com/example/dispositivosmoviles/ReserveClientActivity.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ReserveClientActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.toObject
+import kotlinx.android.synthetic.main.activity_auth.*
 import kotlinx.android.synthetic.main.activity_main_client.*
 import kotlinx.android.synthetic.main.activity_reserve_client.*
 
@@ -37,24 +39,43 @@ class ReserveClientActivity : AppCompatActivity() {
     private fun setup( ) {
         title = "Cliente"
         val bundle: Bundle? = intent.extras
-        val email:String? = bundle?.getString("email")
+        val email: String? = bundle?.getString("email")
+        val db = FirebaseFirestore.getInstance()
+
         holahola.setOnClickListener {
             val homeIn = Intent(this, MainClientActivity::class.java).apply {
-            putExtra("email", email)
-            // putExtra("provider", provider.name)
-            }
-            startActivity(homeIn)
-        }
-        buttondos.setOnClickListener {
-            val homeIn = Intent(this, CrearreservacionClientActivity::class.java).apply {
                 putExtra("email", email)
                 // putExtra("provider", provider.name)
             }
             startActivity(homeIn)
         }
+        buttondos.setOnClickListener {
+            val d = db.collection("Reservacion").document(email.toString())
+            d.get().addOnSuccessListener {
+                val user = it.toObject<ReservacionClassClass>()
+                if (user == null) {
+                    val homeIn = Intent(this, CrearreservacionClientActivity::class.java).apply {
+                        putExtra("email", email)
+                    }
+                    startActivity(homeIn)
+                    // putExtra("provider", provider.name)
+                }
+                else{
+                    showAlertReservacion()
+                }
+            }
+        }
 
     }
 
+    private fun showAlertReservacion(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Ustede ya tiene reservación")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         if (keyCode == KeyEvent.KEYCODE_BACK){
